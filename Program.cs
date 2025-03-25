806eefa7ca97368a0ac010cb2d4aace2c0b29f6f
ozkut <alpozbaykut@gmail.com>
2025-03-25 16:18:03 +0100

    - Chenges to attended mode to allow for different account logins. - Other small changes.

--- Program.cs
+++ Program.cs
@@ -4,16 +4,14 @@ using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Edge;
-using AutoBinger.Properties;
-using OpenQA.Selenium.DevTools.V132.WebAuthn;
 
 namespace AutoBinger
 {
     class Program
     {
-        private static void Exit(string msg = "", bool exit = false)
+        private static void WaitForKeyPress(string msg, bool exit = false)
         {
-            Console.WriteLine(msg + "\nPress any key to exit...");
+            Console.WriteLine(msg + "\nWaiting for key press...");
             Console.ReadKey();
             if (exit)
                 Environment.Exit(Environment.ExitCode);
@@ -23,17 +21,17 @@ namespace AutoBinger
         {
             bool isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 
-            string currentFolder = Path.Combine([Environment.CurrentDirectory, "selenium-manager", (isWindows ? "windows" : "linux")]);
-            _ = Directory.CreateDirectory(currentFolder);
-            await File.WriteAllBytesAsync(Path.Combine(currentFolder, isWindows ? "selenium-manager.exe" : "selenium-manager"), isWindows ? Resources.selenium_manager_win : Resources.selenium_manager_linux);
+            string managerFile = Path.Combine(Directory.CreateDirectory(Path.Combine([Environment.CurrentDirectory, "selenium-manager", (isWindows ? "windows" : "linux")])).FullName, $"selenium-manager{(isWindows ? ".exe" : string.Empty)}");
+            if (!File.Exists(managerFile))
+                await File.WriteAllBytesAsync(managerFile, isWindows ? Properties.Resources.selenium_manager_win : Properties.Resources.selenium_manager_linux);
 
             string filePath = Path.Combine(Environment.GetFolderPath(isWindows ? Environment.SpecialFolder.Desktop : Environment.SpecialFolder.UserProfile), "terms.txt");
             if (!File.Exists(filePath))
-                Exit($"The file \"{filePath}\" does not exits!", true);
+                WaitForKeyPress($"The file \"{filePath}\" does not exits!", true);
 
             string[] terms = await File.ReadAllLinesAsync(filePath);
             if (!int.TryParse(terms[0], out int delay) && delay >= 2)
-                Exit($"Invalid delay on first line of \"{filePath}\"!", true);
+                WaitForKeyPress($"Invalid delay on first line of \"{filePath}\"!", true);
 
             bool isValidUnattendedVar = bool.TryParse(terms[1], out bool isUnattended);
             if (!isValidUnattendedVar)
@@ -46,7 +44,12 @@ namespace AutoBinger
             
             IWebElement cookieButton = driver.FindElement(By.Id("bnp_btn_reject"));
             cookieButton.Click();
-            await Task.Delay(delay / 2);
+
+            if (!isUnattended)
+            {
+                WaitForKeyPress("Ready!");
+                Console.WriteLine("Start");
+            }
 
             for (int i = 1 + (!isValidUnattendedVar ? 0 : 1); i < terms.Length; i++)
             {
@@ -58,7 +61,7 @@ namespace AutoBinger
             }
 
             if (!isUnattended)
-                Exit();
+                WaitForKeyPress("Finished");
             driver.Quit();
         }
     }
